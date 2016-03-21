7eb25174c740263f207c2e14658c5d480068d785
cuspace <cuspace@gmail.com>
2016-03-21 19:18:46 +0900

    Korean is not displayed in Windows, modified to English

--- jakefile.js
+++ jakefile.js
@@ -1,15 +1,15 @@
 (() => {
 	"use strict";
 
-	console.log("\n\n빌드 성공");
+	console.log("\n\nBUILD OK");
 
-	desc("디폴트 테스크");
+	desc("Default Task");
 	task("default", () => {
-		console.log("기본 태스크");
+		console.log("Default Task");
 	});
 
-	desc("첫번재 태스크");
+	desc("First Task");
 	task("task1", () => {
-		console.log("태스크1");
+		console.log("task1");
 	});
 }());
