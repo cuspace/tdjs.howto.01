ca8f7b12243c16c53b05851eecd77b1728a57c74
cuspace <cuspace@gmail.com>
2016-03-21 18:40:05 +0900

    reinstall jake with --ignore-scripts option

--- jakefile.js
+++ jakefile.js
@@ -1,8 +1,15 @@
 (() => {
 	"use strict";
-	
+
 	console.log("\n\n빌드 성공");
+
+	desc("디폴트 테스크");
 	task("default", () => {
 		console.log("기본 태스크");
+	});
+
+	desc("첫번재 태스크");
+	task("task1", () => {
+		console.log("태스크1");
 	});
 }());
