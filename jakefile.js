0ffdcea70a510061bf8b13fbfc5fe4ab2d3414c8
cuspace <cuspace@gmail.com>
2016-03-25 15:50:40 +0900

    Build Scripot gets expected Node version from package.json

--- jakefile.js
+++ jakefile.js
@@ -1,15 +1,19 @@
 (() => {
 	"use strict";
 
-	console.log("\n\nBUILD OK");
-
-	desc("Default Task");
-	task("default", () => {
-		console.log("Default Task");
+	desc("Default");
+	task("default", ['version'], () => {
+		console.log("\n\nBUILD OK");
 	});
 
-	desc("First Task");
-	task("task1", () => {
-		console.log("task1");
+	desc("Check Node Version");
+	task("version", () => {
+		console.log("Checking Node Version: .");
+		var pkgJson = require("./package.json");
+		var expectedVersion = "v" + pkgJson.engines.node;
+		var actualVersion = process.version;
+		if (actualVersion !== expectedVersion)
+			fail("Incorrect node version: expected " + expectedVersion +
+						", but was " + actualVersion);
 	});
 }());
