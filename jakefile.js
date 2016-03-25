78553793c814c3f87476695b6ac37ad0b59f8229
cuspace <cuspace@gmail.com>
2016-03-25 17:41:10 +0900

    Added jshint to jakefile.js

--- jakefile.js
+++ jakefile.js
@@ -1,13 +1,13 @@
-(() => {
+(function() {
 	"use strict";
 
 	desc("Default");
-	task("default", ['version'], () => {
+	task("default", ["version", "lint"], function() {
 		console.log("\n\nBUILD OK");
 	});
 
 	desc("Check Node Version");
-	task("version", () => {
+	task("version", function() {
 		console.log("Checking Node Version: .");
 		var pkgJson = require("./package.json");
 		var semver = require("semver");
@@ -17,5 +17,11 @@
 		if (semver.neq(expectedVersion, actualVersion))
 			fail("Incorrect node version: expected " + expectedVersion +
 						", but was " + actualVersion);
+	});
+
+	desc("Lint JavaScript code");
+	task("lint", {async: true}, function() {
+		console.log("Linting JavaScript code: .");
+		jake.exec("node node_modules/jshint/bin/jshint jakefile.js", {interactive: true}, complete);
 	});
 }());
