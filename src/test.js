841a372b9c3b5d67398ba73d52310e155c5eec5a
cuspace <cuspace@gmail.com>
2016-03-27 16:55:23 +0900

    Example of basic assertEqual

--- src/test.js
+++ src/test.js
@@ -1,4 +1,13 @@
 (function () {
 	"use strict";
 
+	assertEqual(add(3, 4), 6);
+
+	function add(a, b) {
+		return a+b;
+	}
+
+	function assertEqual(actual, expected) {
+		if(expected !== actual) throw new Error("Expected " + expected + ", but got " + actual);
+	}
 }());
