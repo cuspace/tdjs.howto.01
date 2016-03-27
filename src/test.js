eacc97262326f2eaba5c0c877c49f2a644bce02d
cuspace <cuspace@gmail.com>
2016-03-27 17:20:11 +0900

    use chai assert

--- src/test.js
+++ src/test.js
@@ -1,13 +1,11 @@
 (function () {
 	"use strict";
 
-	assertEqual(add(3, 4), 6);
+	var assert = require("chai").assert;
+
+	assert.equal(add(3, 4), 6);
 
 	function add(a, b) {
 		return a+b;
-	}
-
-	function assertEqual(actual, expected) {
-		if(expected !== actual) throw new Error("Expected " + expected + ", but got " + actual);
 	}
 }());
