6fb3ff2f639eac62d827d964e135b9bb411bf60b
cuspace <cuspace@gmail.com>
2016-03-27 15:52:40 +0900

    Can now use './jake.sh run' to run a localhost server

--- jakefile.js
+++ jakefile.js
@@ -1,14 +1,25 @@
-/* globals desc: false, task: false, complete: false, fail: false */
+/* globals jake: false, desc: false, task: false, complete: false, fail: false */
 (function() {
 	"use strict";
 
 	var jshint = require("simplebuild-jshint");
 
+	//**** General-purpose tasks
 	desc("Default");
 	task("default", ["version", "lint"], function() {
 		console.log("\n\nBUILD OK");
 	});
 
+	desc("Run a localhost server");
+	task("run", {async: true}, function() {
+		jake.exec(
+			"node node_modules/http-server/bin/http-server src",
+			{interactive: true	},
+			complete
+		);
+	});
+
+	//**** Supporting tasks
 	desc("Check Node Version");
 	task("version", function() {
 		console.log("Checking Node Version: .");
