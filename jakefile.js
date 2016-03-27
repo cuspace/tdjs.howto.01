025a835a5a9b07d1eb5d6179c28d203f436542a4
cuspace <cuspace@gmail.com>
2016-03-27 14:17:29 +0900

    add jshint options and globals

--- jakefile.js
+++ jakefile.js
@@ -1,3 +1,4 @@
+/* globals desc: false, task: false, complete: false, fail: false */
 (function() {
 	"use strict";
 
@@ -26,7 +27,22 @@
 		process.stdout.write("Linting JavaScript: ");
 		jshint.checkFiles({
 			files: "jakefile.js",
-			options: {},
+			options: {
+				bitwise: true,
+				eqeqeq: true,
+				forin: true,
+				freeze: true,
+				futurehostile: true,
+				latedef: "nofunc",
+				noarg: true,
+				nocomma: true,
+				nonbsp: true,
+				nonew: true,
+				strict: true,
+				undef: true,
+				node: true,
+				browser: true
+			},
 			globals: {}
 		}, complete, fail);
 		//jake.exec("node node_modules/jshint/bin/jshint jakefile.js", {interactive: true}, complete);
