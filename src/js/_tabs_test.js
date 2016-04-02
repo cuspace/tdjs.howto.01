42cc1e03c7daa16de40068756ba68e312d888ed4
cuspace <cuspace@gmail.com>
2016-04-03 02:39:13 +0900

    Created tab library (doesn't do anything)

--- src/js/_tabs_test.js
+++ src/js/_tabs_test.js
@@ -2,10 +2,22 @@
 	"use strict";
 
 	var assert = require("./assert.js");
+	var tabs = require("./tabs.js");
 
 	describe("Tabs", function() {
 
-		it("", function() {
+		it("has an API", function() {
+
+			// red bar : design activity
+			// What is that API gonna look like?
+			// How do we want out tab to run?
+			tabs.initialize();
+
+
+			//var div = document.createElement("div");
+			//div.innerHTML = "This is an example.";
+			//document.body.appendChild(div);
+			//div.parentNode.removeChild(div);
 		});
 
 	});
